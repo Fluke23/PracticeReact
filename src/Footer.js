fc506e22b709963c3ab8a5e51dbcb11cce73eb62
Fluke23 <58130500053@st.sit.kmutt.ac.th>
2017-12-16 00:18:54 +0700

    ทำ state  ตัวอย่าง online time

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,17 @@ import React, {Component} from "react";
 
 
 class Footer extends Component{
+
+    constructor (props){
+        super(props);
+        this.state = {
+            time : 0 
+        }
+    }
     render (){
+        let{time} = this.state;
         return (
-            <div>Footer</div> 
+            <div>Footer online time {time}</div> 
         );
     }
 
