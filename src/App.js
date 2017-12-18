0006052a2400c5bb47883da609890586e95605e5
Fluke23 <piya.dechthanoo@gmail.com>
2017-12-18 21:07:13 +0700

    แก้บัค

--- src/App.js
+++ src/App.js
@@ -4,31 +4,28 @@ import Footer from './Footer'
 
 class App extends Component {
 
-    constructor(props){
-      super(props)
-      this.state = {
-        currentTime : 0
-      }
-      this.handleFooterClicked = this.handleFooterClicked.bind(this)
-    }
+  constructor(props) {
+
+    super(props)
+    this.state = { currentTime: 0 }
+  }
 
 handleFooterClicked(time){
-    this.setState({currentTime : time})
-
-
-    
-  render() {
-    let { currentTime} =this.state
-    return (
-     <div>
-      <Header currentUser = "Fluke" islogIn = {true} /> 
-      <div>currentTime = {currentTime}</div>
-      <Footer onTimeClick = {this.handleFooterClicked}/>
-      </div>
-    );
-  }
-  
-  
-
+  this.setState({currentTime:time })
 }
+
+render(){
+
+  let { currentTime } = this.setState
+  return (
+
+    <div>
+      <Header currentUser="Fluke" islogin />
+      <div>currentTime = {currentTime}</div>
+      <Footer onTimeClick={this.handleFooterClicked} />
+    </div>
+    )
+  }
+}
+
 export default App;
